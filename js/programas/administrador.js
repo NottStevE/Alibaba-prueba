bb403b6cb5c580d260b6b001f76f58fbee916d25
Steve Gomez Boror <stevegomitas3@gmail.com>
2022-08-02 14:20:19 -0600

    se arreglo el login

---
+++ js/programas/administrador.js
@@ -0,0 +1,49 @@
+import { caja_formulario } from "./login.js";
+import { menuI } from "./login.js";
+import { fondo } from "./login.js";
+import { cajaDatos } from "./login.js";
+
+let userTrue = document.getElementById("contraseñaCorrecta");
+let userFalse = document.getElementById("contraseñaIncorrecta");
+let userEmpty = document.getElementById("Nocontraseña");
+
+let passwordTrue = document.getElementById("contraseñaInputCorrecta");
+let PasswordFalse = document.getElementById("contraseñaInputIncorrecta");
+let passwordEmpty = document.getElementById("NocontraseñaInput");
+
+export function interfaz(){
+    caja_formulario.classList.add('desactivar');
+    menuI.classList.add('activar');
+    fondo.classList.add('fondoInterfaz');
+    cajaDatos.classList.add('activar');
+}
+export function usuarioCorrecto(){
+    userTrue.classList.add('contraseñaCorrecta2');
+    userFalse.classList.remove('contraseñaIncorrecta2');
+    userEmpty.classList.remove('Nocontraseña2');
+}
+export function usuarioIncorrecto(){
+    userFalse.classList.add('contraseñaIncorrecta2');
+    userTrue.classList.remove('contraseñaCorrecta2');
+    userEmpty.classList.remove('Nocontraseña2');
+}
+export function colocarUsuario(){
+    userFalse.classList.remove('contraseñaIncorrecta2');
+    userTrue.classList.remove('contraseñaCorrecta2');
+    userEmpty.classList.add('Nocontraseña2');
+}
+export function contraseñaCorrectaa(){
+    passwordTrue.classList.add('contraseñaInputCorrecta2');
+    PasswordFalse.classList.remove('contraseñaInputIncorrecta2');
+    passwordEmpty.classList.remove('NocontraseñaInput2');
+}
+export function contraseñaIncorrecta(){
+    PasswordFalse.classList.add('contraseñaInputIncorrecta2');
+    passwordTrue.classList.remove('contraseñaInputCorrecta2');
+    passwordEmpty.classList.remove('NocontraseñaInput2');
+}
+export function colocarContraseña(){
+    PasswordFalse.classList.remove('contraseñaInputIncorrecta2');
+    passwordTrue.classList.remove('contraseñaInputCorrecta2');
+    passwordEmpty.classList.add('NocontraseñaInput2');
+}
--- js/programas/login.js
+++ js/programas/login.js
@@ -1,20 +1,53 @@
 import { users } from "../DATA/user.js";
+import { interfaz } from "./administrador.js";
+import { usuarioCorrecto } from "./administrador.js";
+import { usuarioIncorrecto } from "./administrador.js";
+import { colocarUsuario } from "./administrador.js";
+import { contraseñaCorrectaa } from "./administrador.js";
+import { contraseñaIncorrecta } from "./administrador.js";
+import { colocarContraseña } from "./administrador.js";
 
-let inputUsuario = document.getElementById("inputUsuario");
-let inputContrasenna = document.querySelector("#inputContrasenna").value;
-let caja_formulario = document.getElementById("caja_formulario");
-
-let menuI = document.getElementById("menu");
+export let caja_formulario = document.getElementById("caja_formulario");
+export let menuI = document.getElementById("menu");
 let botonRegresar = document.getElementById("botonVolver");
-let fondo = document.getElementById("fondo");
-let cajaDatos = document.getElementById("datosAlibaba");
+export let fondo = document.getElementById("fondo");
+export let cajaDatos = document.getElementById("datosAlibaba");
 
 export function loginForm(){
     function loginOff(){
-        caja_formulario.classList.add('desactivar');
-        menuI.classList.add('activar');
-        fondo.classList.add('fondoInterfaz');
-        cajaDatos.classList.add('activar');
+        let userInput = document.querySelector("#inputUsuario").value;
+        let passwordInput = document.querySelector("#inputContrasenna").value;
+
+        for(let i = 0; i <= 6; i++){
+            if(userInput == users[i][1]){
+                usuarioCorrecto();
+                if(passwordInput == users[i][2]){
+                    interfaz();
+                    contraseñaCorrectaa();
+                }else if(passwordInput == ""){
+                    colocarContraseña();
+                }else{
+                    contraseñaIncorrecta();
+                }
+                break;
+            }else if(userInput == ""){
+                colocarUsuario();
+                if(passwordInput == ""){
+                    colocarContraseña();
+                }else{
+                    contraseñaIncorrecta();
+                }
+                break;
+            }else{
+                usuarioIncorrecto();
+                if(passwordInput == ""){
+                    colocarContraseña();
+                }else{
+                    contraseñaIncorrecta();
+                }
+                break;
+            }
+        }
     }
     function loginOn(){
         caja_formulario.classList.remove('desactivar');
